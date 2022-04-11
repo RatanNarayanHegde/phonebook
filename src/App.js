3e2c2a5381301f32408aa1879570f6a186b460c9
RatanNarayanHegde <ratanhegde08@gmail.com>
2022-04-11 09:56:04 +0530

    initial commit and add person function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleAddName = (event) => {
+    event.preventDefault();
+    const found = persons.find((person) => person.name === newName);
+    if (found) {
+      alert(` ${newName} is already present`);
+      setNewName("");
+      return;
+    }
+    const nameObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(nameObject));
+    setNewName("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddName}>
+        <div>
+          name: <input onChange={handleNameChange} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
