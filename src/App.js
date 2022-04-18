ee60cdb0f8542f26868c2c5d1ae1e0656731f52a
RatanNarayanHegde <ratanhegde08@gmail.com>
2022-04-18 18:51:17 +0530

    add delete and update options

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import personService from "./services/persons";
 import { useState, useEffect } from "react";
 
 const Filter = ({ handleChange, value }) => {
@@ -31,16 +31,19 @@ const PersonForm = ({
   );
 };
 
-const Person = ({ person }) => {
+const Person = ({ person, handleDelete }) => {
   return (
     <p>
       {person.name} {person.number}
+      <button onClick={() => handleDelete(person.id)}>Delete</button>
     </p>
   );
 };
 
-const Persons = ({ persons }) => {
-  return persons.map((person) => <Person key={person.name} person={person} />);
+const Persons = ({ persons, handleDelete }) => {
+  return persons.map((person) => (
+    <Person key={person.name} person={person} handleDelete={handleDelete} />
+  ));
 };
 
 const App = () => {
@@ -51,8 +54,8 @@ const App = () => {
 
   useEffect(() => {
     console.log("effect");
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
+    personService.getAll().then((initialPersons) => {
+      setPersons(initialPersons);
     });
   }, []);
 
@@ -71,20 +74,37 @@ const App = () => {
 
   const handleAddName = (event) => {
     event.preventDefault();
+
     const found = persons.find((person) => person.name === newName);
     if (found) {
-      alert(` ${newName} is already present`);
+      if (
+        window.confirm(` ${newName} is already present do you want to update`)
+      ) {
+        // console.log(found);
+        const updatedPerson = { ...found, number: newNumber };
+        personService.update(updatedPerson).then((returnedPerson) => {
+          // console.log(returnedPerson);
+          setPersons(
+            persons.map((person) =>
+              person.id === returnedPerson.id ? returnedPerson : person
+            )
+          );
+          setNewName("");
+          setNewNumber("");
+        });
+      }
       setNewName("");
       setNewNumber("");
       return;
     }
+
     const nameObject = {
       name: newName,
       number: newNumber,
     };
 
-    axios.post("http://localhost:3001/persons", nameObject).then((response) => {
-      setPersons(persons.concat(response.data));
+    personService.create(nameObject).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson));
       setNewName("");
       setNewNumber("");
     });
@@ -93,6 +113,14 @@ const App = () => {
   const personsToDisplay = persons.filter((person) =>
     person.name.toLowerCase().includes(nameFilter)
   );
+
+  const handleDelete = (id) => {
+    if (window.confirm("Are you sure you want to delete")) {
+      personService.remove(id).then((returnedData) => {
+        setPersons(persons.filter((person) => person.id !== id));
+      });
+    }
+  };
 
   return (
     <div>
@@ -107,7 +135,7 @@ const App = () => {
         newNumber={newNumber}
       />
       <h2>Numbers</h2>
-      <Persons persons={personsToDisplay} />
+      <Persons persons={personsToDisplay} handleDelete={handleDelete} />
     </div>
   );
 };
---
+++ src/services/persons.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+const base_url = "http://localhost:3001/persons/";
+
+const getAll = () => {
+  const request = axios.get(base_url);
+  return request.then((resonse) => resonse.data);
+};
+
+const create = (person) => {
+  const request = axios.post(base_url, person);
+  return request.then((response) => response.data);
+};
+
+const remove = (id) => {
+  const request = axios.delete(base_url + id);
+  return request.then((response) => response.data);
+};
+
+const update = (person) => {
+  const request = axios.put(base_url + person.id, person);
+  return request.then((response) => response.data);
+};
+
+export default {
+  getAll,
+  create,
+  remove,
+  update,
+};
